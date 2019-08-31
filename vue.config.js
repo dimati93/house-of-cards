c28b412940de2208360332ab7d885015ec9a202a
Dmytro Petorovych <dimati93@gmail.com>
2019-08-31 23:15:04 +0300

    Replace node-sass package

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ var configureAPI = require('./src/server/api')
 module.exports = {
     devServer: {
         before: configureAPI
-    },    
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                implementation: require('sass'),
+            },
+        },
+    },
 }
